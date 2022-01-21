74527f0bc90fd2016cceeb08551ca4cf6db05a5f
Ryan Cranfill <ryanccranfill@gmail.com>
2022-01-20 19:27:19 -0600

    add setup.py to use as library

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='isegm',
+    version='',
+    packages=find_packages(include=['isegm', 'isegm.*']),
+    include_package_data=True,
+    url='',
+    license='',
+    author='',
+    author_email='',
+    description='',
+)
